4616066d22e29f31fb8b36d8b5f57fc225956871
Fred Ngo <fredngo@gmail.com>
2012-10-15 14:48:34 -0700

    initial implementation

--- s3copy.rb
+++ s3copy.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 
-# s3nukem
+# s3copy
 #
-# original by: Stephen Eley (sfeley@gmail.com)
-# improved by: Robert LaThanh
+# original s3nuke by: Stephen Eley (sfeley@gmail.com)
+# improved s3nuke by: Robert LaThanh
+# modified to s3copy by: Fred Ngo
 #
-# A script to delete Amazon S3 buckets with many objects (millions) quickly by
-# using multiple threads to retrieve and delete the individual objects.
+# A script to copy Amazon S3 buckets with many objects (millions) quickly by
+# using multiple threads to retrieve and copy the individual objects.
 #
-# http://github.com/lathanh/s3nuke
+# http://github.com/fredngo/s3copy
 #
 # Licensed under the Apache License v2.0 (http://apache.org/licenses)
 
@@ -20,21 +21,22 @@ begin
   require 'right_aws'
 rescue LoadError
   puts "Missing the RightAWS gem! Try: sudo gem install right_aws"
-  puts "or for Ruby 1.9: sudo gem install dmarkow-right_aws --source http://gems.github.com"
 end
 
 access_key = nil
 secret_key = nil
-thread_count = 10
+
+thread_count = 1
 max_queue = 10 * 1000
 STDOUT.sync = true
 
-buckets = []
+bucket_from = nil
+bucket_to   = nil
 
 # Parse the command line
 begin
   opts = OptionParser.new
-  opts.banner = "Usage: #{$PROGRAM_NAME} [options] buckets..."
+  opts.banner = "Usage: #{$PROGRAM_NAME} [options] bucket_from bucket_to"
   opts.separator ''
   opts.separator 'Options:'
   opts.on('-a',
@@ -61,11 +63,15 @@ begin
   # opts.separator 'AMAZON_ACCESS_KEY_ID and AMAZON_SECRET_ACCESS_KEY must be set.'
   opts.separator ''
   buckets = opts.parse(ARGV)
+
   if buckets.empty?
-    puts "You must specify at least one bucket to delete!"
+    puts "You must specify bucket_from and bucket_to!"
     puts opts
     exit
   end
+  bucket_from = buckets[0]
+  bucket_to   = buckets[1]
+  
   unless access_key and secret_key
     puts "The --access and --secret options are required!"
     puts opts
@@ -89,68 +95,72 @@ rescue RightAws::AwsError => e
   exit
 end
 
-buckets.each do |bucket|
-  puts "DELETING: #{bucket}"
+puts "START: Copying from #{bucket_from} to #{bucket_to}"
 
-  # Thread management
-  threads = []
-  queue = Queue.new
-  mutex_total = Mutex.new
+# Thread management
+threads = []
+queue = Queue.new
+mutex_total = Mutex.new
 
-  # Tracking variables
-  total_listed = 0
-  total_deleted = 0
+# Tracking variables
+total_listed = 0
+total_copied = 0
 
-  # Key retrieval thread
-  threads << Thread.new do
-    Thread.current[:number] = "CATALOG"
-    puts "Starting catalog thread..."
-    s3 = RightAws::S3Interface.new(access_key, secret_key, :multi_thread => true, :port => 80, :protocol => 'http', :logger => log)
-    prefix = ''
-    begin
-      while queue.length > max_queue
-        sleep 1
-      end
-
-      keys = s3.list_bucket(bucket, :marker => prefix)
-      prefix = keys.last[:key] unless keys.empty?
-      keys.each do |key|
-        queue.enq(key[:key])
-        total_listed += 1
-      end
-    end until keys.empty?
-    thread_count.times {queue.enq(:END_OF_BUCKET)}
-  end
-
-  start_time = Time.now
-  thread_count.times do |count|
-    threads << Thread.new(count) do |number|
-      Thread.current[:number] = number
-      puts "Starting deletion thread #{number}..."
-      s3 = RightAws::S3Interface.new(access_key, secret_key, :multi_thread => true, :port => 80, :protocol => 'http', :logger => log)
-      begin
-        key = queue.deq
-        unless key == :END_OF_BUCKET
-          s3.delete(bucket, key)
-          mutex_total.synchronize {total_deleted += 1}
-          if (rand(1000) == 1)
-            elapsed = Time.now - start_time
-            puts "#{elapsed}: Deleted #{total_deleted} out of #{total_listed}"
-          end
-        end
-      end until (key == :END_OF_BUCKET)
+# Key retrieval thread
+threads << Thread.new do
+  Thread.current[:number] = "CATALOG"
+  puts "Starting catalog thread...\n"
+  s3 = RightAws::S3Interface.new(access_key, secret_key, :multi_thread => true, :port => 80, :protocol => 'http', :logger => log)
+  prefix = ''
+  begin
+    while queue.length > max_queue
+      sleep 1
     end
-  end
 
-  threads.each do |t|
-    begin
-      t.join
-    rescue RuntimeError => e
-      puts "Failure on thread #{t[:number]}: #{e.message}"
+    keys = s3.list_bucket(bucket_from, { :marker => prefix, :'max-keys' => 1000})
+    
+    prefix = keys.last[:key] unless keys.empty?
+    keys.each do |key|
+      queue.enq(key[:key])
+      total_listed += 1
     end
-  end
-
-  # Clean up any stragglers and kill the bucket
-  puts "Bucket #{bucket} deleted!" if mains3.force_delete_bucket(bucket)
-  puts
+  end until keys.empty?
+  thread_count.times {queue.enq(:END_OF_BUCKET)}
 end
+
+start_time = Time.now
+thread_count.times do |count|
+  threads << Thread.new(count) do |number|
+    Thread.current[:number] = number
+    puts "Starting copying thread #{number}...\n"
+    
+    s3 = RightAws::S3Interface.new(access_key, secret_key, :multi_thread => true, :port => 80, :protocol => 'http', :logger => log)
+    begin
+      key = queue.deq
+      unless key == :END_OF_BUCKET
+        
+        acl = s3.get_acl(bucket_from, key)[:object]
+        s3.copy(bucket_from, key, bucket_to, key)
+        s3.put_acl(bucket_to, key, acl)
+
+        mutex_total.synchronize {total_copied += 1}
+        if (total_copied %100 == 0)
+          elapsed = Time.now - start_time
+          puts "#{elapsed}: Copied #{total_copied} out of #{total_listed}"
+        end
+      end
+    end until (key == :END_OF_BUCKET)
+  end
+end
+
+threads.each do |t|
+  begin
+    t.join
+  rescue RuntimeError => e
+    puts "Failure on thread #{t[:number]}: #{e.message}"
+  end
+end
+
+# Clean up any stragglers and kill the bucket
+puts "END: Copied from #{bucket_from} to #{bucket_to}!"
+puts
